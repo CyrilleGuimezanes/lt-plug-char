fe1212f2d68776b8eed27953807628e1b00026e7
Cyrille Guimezanes <cyrille.guimezanes@gmail.com>
2017-01-07 11:21:57 +0100

    - update .h file

--- src/ios/ChartboostPlugin.h
+++ src/ios/ChartboostPlugin.h
@@ -1,10 +1,9 @@
-//Copyright (c) 2015 Luditeam
-//Email: dev@Luditeam.com
+//Copyright (c) 2014 Sang Ki Kwon (Cranberrygame)
+//Email: cranberrygame@yahoo.com
 #import <Cordova/CDV.h>
 #import <Chartboost/Chartboost.h>
-#import <Chartboost/CBNewsfeed.h>
 
-@interface ChartboostPlugin : CDVPlugin <ChartboostDelegate, CBNewsfeedDelegate>
+@interface ChartboostPlugin : CDVPlugin <ChartboostDelegate>
 
 @property NSString *callbackIdKeepCallback;
 //
@@ -19,7 +18,7 @@
 @property BOOL moreAppsAdPreload;
 @property BOOL rewardedVideoAdPreload;
 
-
+- (void) setLicenseKey: (CDVInvokedUrlCommand*)command;
 - (void) setUp:(CDVInvokedUrlCommand*)command;
 - (void) preloadFullScreenAd:(CDVInvokedUrlCommand*)command;
 - (void) showFullScreenAd:(CDVInvokedUrlCommand*)command;
