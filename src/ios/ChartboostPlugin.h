c68eb56049f49a59906d81f8fb69764724edad7a
Cyrille Guimezanes <cyrille.guimezanes@gmail.com>
2017-01-08 22:41:41 +0100

    - MAJ ios

--- src/ios/ChartboostPlugin.h
+++ src/ios/ChartboostPlugin.h
@@ -1,5 +1,7 @@
 //Copyright (c) 2014 Sang Ki Kwon (Cranberrygame)
 //Email: cranberrygame@yahoo.com
+//Homepage: http://cranberrygame.github.io
+//License: MIT (http://opensource.org/licenses/MIT)
 #import <Cordova/CDV.h>
 #import <Chartboost/Chartboost.h>
 
